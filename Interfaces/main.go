0e48af475877580bc8a5024a60ea5ab6f41988eb
mhdiiilham <muhd.iiilham@gmail.com>
2020-06-04 22:47:50 +0700

    Without interfaces

---
+++ Interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// Very custom logic for english
+	return "Hello there!"
+}
+
+func (spanishBot) getGreeting() string {
+	// Very custom logic for Spanish
+	return "Hola!"
+}
